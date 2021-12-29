9dda221622dac5327dbee60e4f5a8ea423c10edc
m-veloper <m_veloper@naver.com>
2021-12-29 20:30:17 +0900

    코드 삭제 및 프로젝트 완료

--- random_restaurant/src/utils/WindowHandler.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-@FunctionalInterface
-public interface WindowHandler {
-    void openWebBrowser() throws URISyntaxException, IOException;
-}
