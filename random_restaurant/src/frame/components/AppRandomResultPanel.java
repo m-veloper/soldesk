a913eb7a293bd1ad199f1277f0911f6fe052442a
민 <58251630+m-veloper@users.noreply.github.com>
2021-12-30 11:47:33 +0900

    데이터 오류 수정

--- random_restaurant/src/frame/components/AppRandomResultPanel.java
+++ random_restaurant/src/frame/components/AppRandomResultPanel.java
@@ -6,6 +6,7 @@ import utils.PanelUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 import static constant.Constants.APP_PANEL_IMG;
 import static constant.PanelCode.APP_RANDOM_RESULT;
@@ -26,12 +27,15 @@ public class AppRandomResultPanel extends JPanel {
         buttonUtils = new ButtonUtils();
         panelUtils = new PanelUtils();
 
+        // 랜덤 데이터를 새로운 객체에 담는다.
+        List<JLabel> result =  panelUtils.getRestaurantResult(status);
+
         // 객체의 사이즈 설정
-        int size = panelUtils.getRestaurantResult(status).size();
+        int size = result.size();
 
         // 객체의 마지막 인덱스에 위치한 url 데이터를 추출
-        JLabel url = panelUtils.getRestaurantResult(status).get(size - 1);
-        panelUtils.getRestaurantResult(status).stream().forEach(s -> {
+        JLabel url = result.get(size - 1);
+        result.stream().forEach(s -> {
             add(s);
         });
 
